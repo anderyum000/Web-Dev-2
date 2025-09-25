e3073041497f7024a9b39c8d3dc28cd32dda1256
anderyum000 <anderyum000@alpinesd.org>
2025-09-24 22:01:30 -0600

    Burger menu

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-const button = document.querySelector("button");
-button.addEventListener("click", submitName);
+const burgermenu = document.querySelector('.burger-menu')
+const nav = document.querySelector('nav')
+let menuOpen = false;
+
+burgermenu .addEventListener('click', () => {
+    if (menuOpen == false) {
+        nav.style.display = "block";
+        menuOpen = true;
+    }
+    else if (menuOpen == true) {
+        nav.style.display = "none";
+        menuOpen = false;
+    }
+});
